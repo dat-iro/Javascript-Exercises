ef76f658b3f07c5286150fb53c82ac25b2b09c1e
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-10-09 01:32:57 +0200

    solution of Callbacks 3

---
+++ exercise.js
@@ -0,0 +1,10 @@
+function repeatHello(callback) {
+    let interval = setInterval(callback, 1000);
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 5000)
+}
+
+let sayHello = () => console.log("Hello");
+
+repeatHello(sayHello);
